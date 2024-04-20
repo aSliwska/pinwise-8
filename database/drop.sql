18c89c57e59489598a12c1c118a957d3ef9d6536
jakar02 <136799016+jakar02@users.noreply.github.com>
2024-04-20 16:56:13 +0200

    Add files via upload
    Helpful drops

---
+++ database/drop.sql
@@ -0,0 +1,21 @@
+--Drop tables
+DROP TABLE IF EXISTS pinwise.categories;
+DROP TABLE IF EXISTS pinwise.pin_type;
+DROP TABLE IF EXISTS pinwise.pin;
+DROP TABLE IF EXISTS pinwise.user;
+
+-- Drop triggers
+DROP TRIGGER IF EXISTS check_admin_permission_trigger ON pinwise.user;
+DROP TRIGGER IF EXISTS validate_email_trigger ON pinwise.user;
+DROP TRIGGER IF EXISTS validate_coordinates ON pinwise.pin;
+DROP TRIGGER IF EXISTS validate_age ON pinwise.user;
+
+-- Drop functions
+DROP FUNCTION IF EXISTS pinwise.check_admin_permission();
+DROP FUNCTION IF EXISTS pinwise.validate_email();
+DROP FUNCTION IF EXISTS pinwise.check_coordinates();
+DROP FUNCTION IF EXISTS pinwise.check_age();
+
+
+
+
